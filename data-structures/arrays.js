b22258d4295a090126608d44a63ee072ed8a83a0
Taechawat Konkaew <luangta_5@hotmail.com>
2021-05-01 17:16:36 +0700

    Add Array data structure

---
+++ data-structures/arrays.js
@@ -0,0 +1,36 @@
+class Array {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    let lastIndex = this.length - 1;
+    const lastItem = this.data[lastIndex];
+    delete this.data[lastIndex];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    this.shiftItems(index);
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+}
