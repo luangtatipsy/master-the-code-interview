66e9b48f738b5c258dbce37a0d41689d8abfd197
Taechawat Konkaew <luangta_5@hotmail.com>
2021-05-04 15:44:56 +0700

    Remove debug codes

--- data-structures/trees/BinarySearchTree.js
+++ data-structures/trees/BinarySearchTree.js
@@ -139,19 +139,3 @@ function traverse(node) {
   tree.right = node.right === null ? null : traverse(node.right);
   return tree;
 }
-
-//       9
-//    4     20
-//  1   6  15  170
-
-const tree = new BinarySearchTree();
-tree.insert(9);
-tree.insert(4);
-tree.insert(6);
-tree.insert(20);
-tree.insert(170);
-tree.insert(15);
-tree.insert(1);
-tree.insert(8);
-tree.remove(1);
-console.log(JSON.stringify(traverse(tree.root)));
